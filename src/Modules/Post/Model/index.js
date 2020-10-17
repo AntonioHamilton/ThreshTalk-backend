82137b1f5277eae09c40433376d0ac9fbb514df8
AntonioHamilton <antoniohamiltonfreitas@outlook.com>
2020-10-16 21:06:55 -0300

    feat(Post Model): Post collection was created

---
+++ src/Modules/Post/Model/index.js
@@ -0,0 +1,33 @@
+const { Mongoose } = require('mongoose');
+
+const mongoose = require('mongoose');
+var uuid = require('node-uuid');
+
+const PostSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      unique: true,
+      required: true,
+      default: uuid.v1(),
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    comments: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: {
+      updatedAt: true,
+      createdAt: true,
+    },
+  }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
