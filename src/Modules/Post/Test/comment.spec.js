1e79d7e8eef2f801d9c0a8f31bf12a6beab6ca4b
AntonioHamilton <antoniohamiltonfreitas@outlook.com>
2020-10-18 00:01:28 -0300

    feat(comment tests)

---
+++ src/Modules/Post/Test/comment.spec.js
@@ -0,0 +1,77 @@
+const request = require('supertest');
+const app = require('../../../Config/app')();
+const databaseHelper = require('../../../Helpers/databaseHelper');
+const uuid = require('node-uuid');
+
+const Post = require('../Model');
+
+describe('Unit tests of posts', () => {
+  beforeAll(async () => {
+    await databaseHelper.openConnection();
+    await request(app).post('/posts').send({
+      message: 'any_message',
+    });
+  });
+
+  afterAll(async () => {
+    await Post.deleteMany({});
+    await databaseHelper.closeConnection();
+  });
+
+  describe('addComment tests', () => {
+    test('Should add a new comment', async () => {
+      const allPosts = await request(app).get('/posts');
+      const id = allPosts.body[0].id;
+      const res = await request(app).post(`/posts/${id}/comments`).send({
+        comment: 'any_comment',
+      });
+      expect(res.status).toBe(201);
+    });
+
+    test('Should not add a new comment without a comment', async () => {
+      const allPosts = await request(app).get('/posts');
+      const id = allPosts.body[0].id;
+      const res = await request(app).post(`/posts/${id}/comments`).send({
+        comment: '',
+      });
+      expect(res.status).toBe(500);
+    });
+
+    test('Should not add a new comment in a nonexistent post', async () => {
+      const newId = uuid.v1();
+      const res = await request(app).post(`/posts/${newId}/comments`).send({
+        comment: 'teste',
+      });
+      expect(res.status).toBe(404);
+    });
+
+    test('Should not get post without a id non-typed as uuid', async () => {
+      const newId = 1;
+      const res = await request(app).post(`/posts/${newId}/comments`).send({
+        comment: 'teste',
+      });
+      expect(res.status).toBe(500);
+    });
+  });
+
+  describe('readComments tests', () => {
+    test('Should get all comments of a post', async () => {
+      const allPosts = await request(app).get('/posts');
+      const id = allPosts.body[0].id;
+      const res = await request(app).get(`/posts/${id}/comments`);
+      expect(res.status).toBe(200);
+    });
+
+    test('Should not create a comment in a nonexistent post', async () => {
+      const newId = uuid.v1();
+      const res = await request(app).get(`/posts/${newId}/comments`);
+      expect(res.status).toBe(404);
+    });
+
+    test('Should not get post without a id non-typed as uuid', async () => {
+      const newId = 1;
+      const res = await request(app).get(`/posts/${newId}/comments`);
+      expect(res.status).toBe(500);
+    });
+  });
+});
--- src/Modules/Post/Test/post.spec.js
+++ src/Modules/Post/Test/post.spec.js
@@ -24,6 +24,15 @@ describe('Unit tests of posts', () => {
       expect(res.status).toBe(201);
     });
 
+    test('Should not create a post with a customized id', async () => {
+      const res = await request(app).post('/posts').send({
+        id: uuid.v1(),
+        image: 'any_string_image',
+        message: 'any_message',
+      });
+      expect(res.status).toBe(500);
+    });
+
     test('Should not create a post without message', async () => {
       const res = await request(app).post('/posts').send({
         image: 'any_string_image',
@@ -40,17 +49,23 @@ describe('Unit tests of posts', () => {
   });
 
   describe('readOne post tests', () => {
-    test('Should get id of one post and return it', async () => {
+    test('Should create a post and return it', async () => {
       const allPosts = await request(app).get('/posts');
       const id = allPosts.body[0].id;
       const res = await request(app).get(`/posts/${id}`);
       expect(res.status).toBe(200);
     });
 
-    test('Should not get post with an nonexistent', async () => {
+    test('Should not get post with an nonexistent post id', async () => {
       const id = uuid.v1();
       const res = await request(app).get(`/posts/${id}`);
       expect(res.status).toBe(404);
+    });
+
+    test('Should not get post without a id non-typed as uuid', async () => {
+      const id = 1;
+      const res = await request(app).get(`/posts/${id}`);
+      expect(res.status).toBe(500);
     });
   });
 });
